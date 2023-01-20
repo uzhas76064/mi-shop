8f06806616d29bfa24825ffae76759d552a846aa
Hvatov Maksim <uzhas76064@gmail.com>
2023-01-20 11:39:29 +0300

    Moved files and added auth.module.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,9 @@ const path = require('path');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    entry: {
-        main: './index.js',
-        catalog: './catalog.js',
-        admin: './admin.js',
-    },
+    entry: './index.js',
     output: {
-        filename: './js/[name].js',
+        filename: './js/main.js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
