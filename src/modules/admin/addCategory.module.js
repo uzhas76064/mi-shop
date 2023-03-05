1d9833490a0d17a1c1965f227670ac18869ad35b
Hvatov Maksim <uzhas76064@gmail.com>
2023-03-05 15:56:44 +0300

    Added post method to put categoryData on the server

--- src/modules/admin/addCategory.module.js
+++ src/modules/admin/addCategory.module.js
@@ -1,3 +1,68 @@
+import {postData, getData} from "../api.module";
+
 export const addCategory = () => {
-    console.log('addCategory')
+    const nameInput = document.getElementById('category-name')
+    const imgInput = document.getElementById('category-image')
+    const addBtn = document.getElementById('category-add-btn')
+
+    // Данные категории для добавления
+    const categoryData = {
+        name: '',
+        preview: ''
+    }
+
+    // Проверка вводимых значений на пустоту
+    const checkInputValues = () => {
+        if (nameInput.value === '' || imgInput.value === '') {
+            addBtn.disabled = true;
+        } else {
+            addBtn.disabled = false;
+        }
+    }
+
+    // Событие ввода имени категории
+    nameInput.addEventListener('input', () => {
+        categoryData.name = nameInput.value
+        checkInputValues()
+    })
+
+    // Слобытие загрузки картинки категории
+    imgInput.addEventListener('input', () => {
+        const file = imgInput.files[0] // Получение объекта файла, т.к. он единственный, то обращаемся по индексу 0
+        const fileReader = new FileReader() // Ассинхронное чтение файла
+
+        // Проверка файла по нужному типу
+        if (file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'iamge/jpg') {
+            // Событие при успрешной закгрузки нужного файла
+            fileReader.onload = function loadImg()  {
+                categoryData.preview = fileReader.result;
+            }
+
+            // Событие ошибки при загрузке
+            fileReader.onerror = function errorImg () {
+                categoryData.preview = ''
+            }
+            fileReader.readAsDataURL(file)
+        } else {
+            imgInput.value = ''
+        }
+        checkInputValues()
+    })
+
+    // Обработка клика по кнопке добавления категории
+    addBtn.addEventListener('click', () => {
+        postData('/categories', { // Ложим на сервер данные из инпутов
+            method: 'POST',
+            body: JSON.stringify(categoryData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(data => { // Получаем данные категорий
+            getData('/categories').then(data => {
+                console.log(data)
+            })
+        })
+    })
+
+    checkInputValues()
 }
--- src/modules/api.module.js
+++ src/modules/api.module.js
@@ -3,3 +3,7 @@ const server = 'http://localhost:3001'
 export const getData = (path) => {
     return fetch(server + path).then(response => response.json())
 }
+
+export const postData = (path, data) => {
+    return fetch(server + path, data)
+}
